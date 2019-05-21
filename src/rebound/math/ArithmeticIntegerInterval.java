d551e88c78f9bb21884b3e32fd642cfc55b03020
PuppyPi <puppyofcodez@gmail.com>
2019-05-21 20:08:20 +0000

    Oh yeah also this is new (forgot to add it to the index last commit ^^' )

---
+++ src/rebound/math/ArithmeticIntegerInterval.java
@@ -0,0 +1,139 @@
+package rebound.math;
+
+import javax.annotation.concurrent.Immutable;
+import rebound.exceptions.OverflowException;
+
+@Immutable
+public class ArithmeticIntegerInterval
+{
+	protected final long start;
+	protected final long size;
+	
+	
+	public ArithmeticIntegerInterval(long start, long size)
+	{
+		this.start = start;
+		this.size = size;
+		
+		if (size < 0)
+			throw new OverflowException();
+		
+		if (start + size < start)
+			throw new OverflowException();
+	}
+	
+	
+	public long getStart()
+	{
+		return this.start;
+	}
+	
+	public long getSize()
+	{
+		return this.size;
+	}
+	
+	public long getPastEnd()
+	{
+		return this.start + this.size;
+	}
+	
+	
+	public boolean isEmpty()
+	{
+		return getSize() == 0;
+	}
+	
+	
+	public boolean isValueInUnderlyingInRange(long valueInUnderlying)
+	{
+		//return valueInUnderlying >= start && valueInUnderlying < start + size;
+		
+		valueInUnderlying -= this.start;
+		return valueInUnderlying >= 0 && valueInUnderlying < this.size;
+	}
+	
+	
+	
+	
+	
+	public ArithmeticIntegerInterval subinterval(long start, long size)
+	{
+		if (size < 0)
+			throw new IllegalArgumentException();
+		if (start < 0)
+			throw new IndexOutOfBoundsException();
+		if (start + size > this.size)
+			throw new IndexOutOfBoundsException();
+		
+		if (start == 0 && size == this.getSize())
+			return this;
+		else
+			return new ArithmeticIntegerInterval(this.start + start, size);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	public ArithmeticIntegerInterval subintervalByExclusiveBound(long start, long end)
+	{
+		return subinterval(start, end - start);
+	}
+	
+	public ArithmeticIntegerInterval subintervalToEnd(long start)
+	{
+		return subinterval(start, this.size - start);
+	}
+	
+	public ArithmeticIntegerInterval subintervalFromBeginning(long sizeOrExclusiveEndingBound)
+	{
+		return subinterval(0, sizeOrExclusiveEndingBound);
+	}
+
+
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (size ^ (size >>> 32));
+		result = prime * result + (int) (start ^ (start >>> 32));
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ArithmeticIntegerInterval other = (ArithmeticIntegerInterval) obj;
+		
+		if (this.isEmpty())
+			return other.isEmpty();
+		else if (other.isEmpty())  // && !this.isEmpty()
+			return false;
+		
+		if (size != other.size)
+			return false;
+		if (start != other.start)
+			return false;
+		
+		return true;
+	}
+
+
+	@Override
+	public String toString()
+	{
+		return "["+getStart()+", "+getPastEnd()+")";
+	}
+}
