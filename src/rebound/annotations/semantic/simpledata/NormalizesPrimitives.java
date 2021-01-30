b0024b34faadc5dc50208fee643ca6027948d3d2
PuppyPi <puppyofcodez@gmail.com>
2021-01-30 14:54:48 +0000

    Minor doc changes X3

--- src/rebound/annotations/semantic/simpledata/NormalizesPrimitives.java
+++ src/rebound/annotations/semantic/simpledata/NormalizesPrimitives.java
@@ -5,13 +5,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /**
- * Something tagged with this converts some primitive wrappers into equivalent wrappers of wider types!
+ * Something tagged with this converts some primitive wrappers into equivalent wrappers of some wider types!
  * Eg, byte/short -> int/long, or float -> double
  * 
  * ^w^
  * 
  * 
- * + Note: if you only do this internally, and the normalization is unseen and functionally affects nothing, you don't have to tag it with this annotation :3
+ * + Note: if you only do this internally, and the normalization is unseen and functionally affects nothing (eg, the normalized value doesn't get returned), you don't have to tag it with this annotation :3
  * 
  * @author RP
  */
