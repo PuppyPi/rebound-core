60bb250ff47360c714fd7a3787e23d48d04f2e45
PuppyPi <puppyofcodez@gmail.com>
2021-02-15 07:08:23 +0000

    Added the Transactional TransientError trait interface, since that's completely generic (it might have nothing to do with conventional "databases" per-se, just failable-operations-to-be-repeated-on-transient-failure!)

---
+++ src/rebound/db/TransientTransactionalException.java
@@ -0,0 +1,21 @@
+package rebound.db;
+
+import rebound.annotations.semantic.StaticTraitPredicate;
+import rebound.annotations.semantic.TraitPredicate;
+
+public interface TransientTransactionalException
+{
+	//<<< tp TransientTransactionalException
+	@TraitPredicate
+	public default boolean isTransientTransactionalException()
+	{
+		return true;
+	}
+	
+	@StaticTraitPredicate
+	public static boolean is(Object x)
+	{
+		return x instanceof TransientTransactionalException && ((TransientTransactionalException)x).isTransientTransactionalException();
+	}
+	//>>>
+}
