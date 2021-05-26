d81af7896d1f933dd2889438dae222d179ba566f
PuppyPi <puppyofcodez@gmail.com>
2021-05-26 02:26:44 +0000

    NonsemanticOrdinals X3

---
+++ src/rebound/annotations/semantic/NonsemanticOrdinals.java
@@ -0,0 +1,23 @@
+/*
+ * Created on May 19, 2014
+ * 	with the GREAT AND POWERFUL Eclipse(c)
+ */
+package rebound.annotations.semantic;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Explicit opposite of {@link SemanticOrdinals} for being explicit X3
+ * 
+ * @author Puppy Pie ^_^
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface NonsemanticOrdinals
+{
+}
