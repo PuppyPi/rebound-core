4c2ba61afb402059021e29eafc669bca8317d7d5
PuppyPi <puppyofcodez@gmail.com>
2022-07-02 02:03:43 +0000

    Make ContainerAndRelativePathWithinIt hashable :3

--- src/rebound/file/ContainerAndRelativePathWithinIt.java
+++ src/rebound/file/ContainerAndRelativePathWithinIt.java
@@ -2,7 +2,9 @@ package rebound.file;
 
 import static java.util.Objects.*;
 import javax.annotation.Nonnull;
+import rebound.annotations.semantic.operationspecification.HashableType;
 
+@HashableType  //If C is!
 public class ContainerAndRelativePathWithinIt<C>
 {
 	protected final C containingNamespace;
@@ -25,5 +27,53 @@ public class ContainerAndRelativePathWithinIt<C>
 	public String getRelativePathWithinNamespace()
 	{
 		return this.relativePathWithinNamespace;
+	}
+	
+	
+	
+	
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((containingNamespace == null) ? 0 : containingNamespace.hashCode());
+		result = prime * result + ((relativePathWithinNamespace == null) ? 0 : relativePathWithinNamespace.hashCode());
+		return result;
+	}
+	
+	
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ContainerAndRelativePathWithinIt other = (ContainerAndRelativePathWithinIt) obj;
+		if (containingNamespace == null)
+		{
+			if (other.containingNamespace != null)
+				return false;
+		}
+		else if (!containingNamespace.equals(other.containingNamespace))
+			return false;
+		if (relativePathWithinNamespace == null)
+		{
+			if (other.relativePathWithinNamespace != null)
+				return false;
+		}
+		else if (!relativePathWithinNamespace.equals(other.relativePathWithinNamespace))
+			return false;
+		return true;
+	}
+	
+	
+	@Override
+	public String toString()
+	{
+		return "ContainerAndRelativePathWithinIt [containingNamespace=" + containingNamespace + ", relativePathWithinNamespace=" + relativePathWithinNamespace + "]";
 	}
 }
