49c44a370aeb6b1c0ca8fe5cbcc3c6a81225fb19
PuppyPi <puppyofcodez@gmail.com>
2022-07-11 00:29:25 +0000

    Yet another teeny thing :3

---
+++ src/rebound/io/PerformanceFlagSmallReadsOrWritesAreOk.java
@@ -0,0 +1,18 @@
+/*
+ * Created on May 18, 2013
+ * 	with the GREAT AND POWERFUL Eclipse(c)
+ */
+package rebound.io;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
+/**
+ * Eg, {@link BufferedReader} / {@link BufferedWriter} / {@link BufferedInputStream} / {@link BufferedOutputStream}
+ * @author RProgrammer
+ */
+public interface PerformanceFlagSmallReadsOrWritesAreOk
+{
+}
