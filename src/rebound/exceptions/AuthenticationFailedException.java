9c6f05ea9fdc0a4dc7acc7435d799ba7a9706ecc
PuppyPi <puppyofcodez@gmail.com>
2022-09-05 05:01:38 +0000

    Make it general like the others :3

--- src/rebound/exceptions/AuthenticationFailedException.java
+++ src/rebound/exceptions/AuthenticationFailedException.java
@@ -5,5 +5,25 @@ import java.io.IOException;
 public class AuthenticationFailedException
 extends IOException
 {
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1l;
+	
+	public AuthenticationFailedException()
+	{
+		super();
+	}
+	
+	public AuthenticationFailedException(String message)
+	{
+		super(message);
+	}
+	
+	public AuthenticationFailedException(Throwable cause)
+	{
+		super(cause);
+	}
+	
+	public AuthenticationFailedException(String message, Throwable cause)
+	{
+		super(message, cause);
+	}
 }
