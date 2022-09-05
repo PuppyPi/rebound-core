bccca133174f3940a5e3be6a21f21e193c17fa75
PuppyPi <puppyofcodez@gmail.com>
2022-09-05 05:01:15 +0000

    Really, this is a pretty common thing; so let's make it be here for a common sharing across APIs :3 (like the other exceptions X3 )

---
+++ src/rebound/exceptions/AuthenticationFailedException.java
@@ -0,0 +1,9 @@
+package rebound.exceptions;
+
+import java.io.IOException;
+
+public class AuthenticationFailedException
+extends IOException
+{
+	private static final long serialVersionUID = 1L;
+}
