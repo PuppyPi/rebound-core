89fc56c6a47497619edf1e6e154d802fc3362d15
PuppyPi <puppyofcodez@gmail.com>
2022-09-19 01:12:51 +0000

    Just to be clear X3

--- src/rebound/util/SmallIntegerAllocationManager.java
+++ src/rebound/util/SmallIntegerAllocationManager.java
@@ -2,6 +2,9 @@ package rebound.util;
 
 import javax.annotation.Nonnegative;
 
+/**
+ * Note: things rely on the indexes produced/provided being Non-Negative so that is a guaranteed aspect of this API that will never change.
+ */
 public interface SmallIntegerAllocationManager
 {
 	public @Nonnegative int allocateNew();
