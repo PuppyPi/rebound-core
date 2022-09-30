758f7a72fbf98526729c298f1fe64c1a6cdc2087
PuppyPi <puppyofcodez@gmail.com>
2022-09-30 22:58:49 +0000

    Better capacity-restricted collections :>>>

---
+++ src/rebound/concurrency/blocks/ResizeableCapacityRestrictedCollection.java
@@ -0,0 +1,27 @@
+package rebound.concurrency.blocks;
+
+import javax.annotation.Nonnegative;
+import javax.annotation.concurrent.ThreadSafe;
+import rebound.annotations.semantic.temporal.ConstantReturnValue;
+import rebound.util.collections.CapacityRestrictedCollection;
+
+public interface ResizeableCapacityRestrictedCollection<E>
+extends CapacityRestrictedCollection<E>
+{
+	/**
+	 * This is the implementation-decided (inclusive) maximum that the user can set the user-decided maximum to with {@link #setCapacity(int)}.
+	 * @return {@link Integer#MAX_VALUE} for "infinity" because {@link #size() we're already restricted to that anyway} XD''
+	 */
+	@ConstantReturnValue
+	@Nonnegative
+	public int getMaxCapacity();
+	
+	
+	/**
+	 * @throws IllegalArgumentException  if the provided capacity is > {@link #getMaxCapacity()}  (not >=)
+	 */
+	public void setCapacity(@Nonnegative int capacity) throws IllegalArgumentException;
+	
+	@Override
+	public @Nonnegative int getCapacity();
+}
--- src/rebound/util/collections/CapacityRestrictedCollection.java
+++ src/rebound/util/collections/CapacityRestrictedCollection.java
@@ -8,6 +8,9 @@ public interface CapacityRestrictedCollection<E>
 extends Collection<E>
 {
 	/**
+	 * This is the (inclusive) maximum of {@link #size()}.
+	 * Once the collection is at capacity ({@link #size()} == {@link #getCapacity()}), any attempts to {@link #add(Object) add} more elements will result in a {@link CapacityReachedException} being thrown and nothing done.
+	 * 
 	 * @return {@link Integer#MAX_VALUE} for "infinity" because {@link #size() we're already restricted to that anyway} XD''
 	 */
 	public @Nonnegative int getCapacity();
@@ -16,6 +19,9 @@ extends Collection<E>
 	@Override
 	public boolean add(E e) throws CapacityReachedException;
 	
+	/**
+	 * @throws CapacityReachedException  if this is thrown, nothing was done (not a single element added) and you can cleanly try again later when conditions are different!
+	 */
 	@Override
 	public boolean addAll(Collection<? extends E> c) throws CapacityReachedException;
 }
