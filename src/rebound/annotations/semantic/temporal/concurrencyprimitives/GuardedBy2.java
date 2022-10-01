40603497f23a1386621475d017a15d13819442c9
PuppyPi <puppyofcodez@gmail.com>
2022-10-01 21:42:35 +0000

    Oh right, I had this realization a few days ago. Sheesh, concurrency is crazy X'D

--- src/rebound/annotations/semantic/temporal/concurrencyprimitives/GuardedBy2.java
+++ src/rebound/annotations/semantic/temporal/concurrencyprimitives/GuardedBy2.java
@@ -10,6 +10,11 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import javax.annotation.concurrent.GuardedBy;
 
+//Todo oh perhaps we should have something that says the *contents* of a thing are guarded XD''
+// (since a final field doesn't need to be guarded *itself*, it's the fields *inside the object* that it points to that we're talking about being guarded!!)
+// (oof that's a lot of scanning code to figure out which I meant, (with @GuardedBy as well as @GuardedBy2!) x'D )
+// (and also, it would never be enough, because whether things inside *that* are guarded by it as well and so on and so on can't easily be specified!! X'D )
+
 /**
  * Same as {@link GuardedBy}, but without the {@link Target} restrictions :P
  * (*cough* constructors and local variables *cough* XD'' )
