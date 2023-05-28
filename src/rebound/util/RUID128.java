46877de37338dc1e2b4ee40a3892723a464ac744
PuppyPi <puppyofcodez@gmail.com>
2023-05-28 16:05:40 +0000

    Finally added RUID128 as a proper class XD'

---
+++ src/rebound/util/RUID128.java
@@ -0,0 +1,49 @@
+package rebound.util;
+
+public class RUID128
+{
+	protected final long lowBits, highBits;
+	
+	public RUID128(long lowBits, long highBits)
+	{
+		this.lowBits = lowBits;
+		this.highBits = highBits;
+	}
+	
+	public long getLowBits()
+	{
+		return lowBits;
+	}
+	
+	public long getHighBits()
+	{
+		return highBits;
+	}
+	
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (highBits ^ (highBits >>> 32));
+		result = prime * result + (int) (lowBits ^ (lowBits >>> 32));
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RUID128 other = (RUID128) obj;
+		if (highBits != other.highBits)
+			return false;
+		if (lowBits != other.lowBits)
+			return false;
+		return true;
+	}
+}
