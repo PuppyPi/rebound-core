f915a40c52c8892339afbe559c1e9a5a8a41a6eb
PuppyPi <puppyofcodez@gmail.com>
2023-10-28 06:31:46 +0000

    That TODO is in the address-specific interfaces :3

--- src/rebound/util/collections/prim/NonuniformMethodsForBooleanList.java
+++ src/rebound/util/collections/prim/NonuniformMethodsForBooleanList.java
@@ -5,8 +5,6 @@ import rebound.annotations.hints.ImplementationTransparency;
 import rebound.annotations.hints.IntendedToBeSubclassedImplementedOrOverriddenByApiUser;
 import rebound.util.collections.prim.PrimitiveCollections.DefaultToArraysBooleanCollection;
 
-//TODO Elementwise boolean operations between BooleanLists!!  AND, OR, NOT, XOR!  \:D/
-
 @ImplementationTransparency  //This only exists to keep the bulk functions exactly uniform and correspondent :3     This might be removed soon XD''
 public interface NonuniformMethodsForBooleanList
 extends DefaultToArraysBooleanCollection, NonuniformMethodsForBooleanList32, NonuniformMethodsForBooleanList64, NonuniformMethodsForBooleanListMiscellaneous
