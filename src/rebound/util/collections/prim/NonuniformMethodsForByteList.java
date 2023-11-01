c7c70f31e935687bcf562bcd211952c275bd5c2d
PuppyPi <puppyofcodez@gmail.com>
2023-11-01 00:33:24 +0000

    Moving this out in preparation to super it up too! :D

---
+++ src/rebound/util/collections/prim/NonuniformMethodsForByteList.java
@@ -0,0 +1,11 @@
+package rebound.util.collections.prim;
+
+import rebound.annotations.hints.ImplementationTransparency;
+import rebound.util.collections.prim.PrimitiveCollections.DefaultToArraysByteCollection;
+
+@ImplementationTransparency  //This class only exists to keep the bulk functions exactly uniform and correspondent :3     This might be removed soon XD''
+public interface NonuniformMethodsForByteList
+extends DefaultToArraysByteCollection
+{
+	
+}
--- src/rebound/util/collections/prim/PrimitiveCollections.java
+++ src/rebound/util/collections/prim/PrimitiveCollections.java
@@ -643,14 +643,6 @@ public class PrimitiveCollections
 	
 	
 	@ImplementationTransparency  //This class only exists to keep the bulk functions exactly uniform and correspondent :3     This might be removed soon XD''
-	public static interface NonuniformMethodsForByteList
-	extends DefaultToArraysByteCollection
-	{
-		
-	}
-	
-	
-	@ImplementationTransparency  //This class only exists to keep the bulk functions exactly uniform and correspondent :3     This might be removed soon XD''
 	public static interface NonuniformMethodsForCharacterList
 	extends CharSequence, DefaultToArraysCharacterCollection
 	{
