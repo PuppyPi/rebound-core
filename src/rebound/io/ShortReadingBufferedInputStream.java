8e2f79ae56a32c9a91a207faa33be14712321294
PuppyPi <puppyofcodez@gmail.com>
2023-12-18 06:46:02 +0000

    Embiggened the docs :333

--- src/rebound/io/ShortReadingBufferedInputStream.java
+++ src/rebound/io/ShortReadingBufferedInputStream.java
@@ -1,21 +1,40 @@
 package rebound.io;
 
 import static rebound.math.SmallIntegerMathUtilities.*;
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import rebound.annotations.semantic.simpledata.Positive;
 
 /**
- * This is useful for, say, tunnelling where you MUST NOT use BufferedInputStream because that can TOTALLY mess up the flush information by waiting for an indefinite
+ * This is useful for, say, tunnelling where you MUST NOT use {@link BufferedInputStream} because that can TOTALLY mess up the flush information by waiting for an indefinite
  * amount of time before it fills up its buffer.  The sender of the data might NEED that data to be flushed straight to the other side!  Possibly because the
  * other side is waiting for it and won't send its data until it gets it!!  And if we're waiting on it to send more to fill up the buffer, and its waiting on the
  * other side, and the other side is waiting on us, that's a deadlock!!!
  * 
- * So ***absolutely only*** use ShortReadingBufferedInputStream, not BufferedInputStream because it will never make more than one read() call to the underlying
- * stream, it will only allow it to be extra big, and rely on the underlying stream to return a short read rather than block, and then buffer any extra data
- * so that future calls will just use that up directly.
+ * Again, for clarity,
+ * Say you have Side A and Side B, and there's full-duplex communication between them (information can pass in both directions completely arbitrarily, in a multithreaded
+ * way!).  Now say Side B is like a Web Server, just responding to what Side A sends and sending something as soon as it receives a request.  And say you're a
+ * tunnel/relay/proxy between Side A and B, doing nothing but forwarding the traffic in both direction (eg, to get around a NAT router on Side A or something perhaps).
+ * So look at this possible situation!!:
+ * • Side A sends a request that's say, 100 bytes.
+ * • Your {@link BufferedInputStream} blocks, read()ing and waiting for Side A to send enough bytes to fill up its buffer.
+ * • So you're not forwarding it to Side B!
+ * • Side B blocks, sends nothing until it receives some request message!
+ * • Side A *might* send more data—perhaps even enough to fill up your {@link BufferedInputStream} buffer!  *But not until it gets a response from Side B*
+ * • So you're stuck waiting on Side A, Side A is stuck waiting on Side B, Side B is stuck waiting on you!!  **Deadlock!!!*
  * 
- * Ie, a read() call to it only makes 0 or 1 read() calls to the underlying (possibly of different parameters, but still), never more than 1!
+ * • {@link ShortReadingBufferedInputStream} breaks this deadlock by returning from its {@link #read(byte[], int, int) read()} method with no more than a single read
+ * from the underlying pipe from Side A!  In other words, it doesn't try to *fill* its own buffer, it just offers the underlying stream/pipe the opportunity to fill
+ * the buffer in a single read() call, and *if* it gets more than you asked this class for, *then* it will buffer, and so you can make a thousand tiny read() calls
+ * (like for single bytes or 16/32/64-bit integers or such) and this class will handle it performantly, reading, say, chewing on say, single TCP packet from the
+ * underlying stream, *but it will never try to read more than one TCP packet!!* (technically, more than one read() call to the underlying stream!).
+ * 
+ * So ***absolutely only*** use {@link ShortReadingBufferedInputStream}, not {@link BufferedInputStream} for forwarding duplex traffic, because it will never make
+ * more than one read() call to the underlying stream, it will only allow it to be extra big, and rely on the underlying stream to return a short read rather than
+ * block, and then buffer any extra data so that future calls will just use that up directly.
+ * 
+ * Ie, a read() call to this class only makes 0 or 1 read() calls to the underlying stream, never more than 1! (possibly of different parameters, but still)
  */
 public class ShortReadingBufferedInputStream
 extends InputStream
