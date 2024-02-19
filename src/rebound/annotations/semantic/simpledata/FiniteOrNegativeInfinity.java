b0728b730abf715ea4e15e93cc5d8905af0691d9
PuppyPi <puppyofcodez@gmail.com>
2024-02-19 02:19:37 -0600

    Pfft. why didn't we have this? XD

---
+++ src/rebound/annotations/semantic/simpledata/FiniteOrNegativeInfinity.java
@@ -0,0 +1,18 @@
+/*
+ * Created on Apr 14, 2014
+ * 	with the GREAT AND POWERFUL Eclipse(c)
+ */
+package rebound.annotations.semantic.simpledata;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Constrast with {@link Finite}
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FiniteOrNegativeInfinity
+{
+}
