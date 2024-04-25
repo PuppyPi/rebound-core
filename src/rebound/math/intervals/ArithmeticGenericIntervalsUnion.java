af1a26abff5a6141ded1d3c2bbc14c04bf2e4301
PuppyPi <puppyofcodez@gmail.com>
2024-04-25 02:17:25 +0000

    This is Generic, so it can't rely on hashcode/equals!

--- src/rebound/math/intervals/ArithmeticGenericIntervalsUnion.java
+++ src/rebound/math/intervals/ArithmeticGenericIntervalsUnion.java
@@ -4,8 +4,12 @@ import static java.util.Objects.*;
 import static rebound.util.collections.CollectionUtilities.*;
 import java.util.List;
 import javax.annotation.Nonnull;
+import javax.annotation.concurrent.Immutable;
+import rebound.annotations.semantic.operationspecification.UnhashableType;
 import rebound.annotations.semantic.simpledata.Emptyable;
 
+@UnhashableType  //because how do we know what things are in the member intervals!?
+@Immutable
 public class ArithmeticGenericIntervalsUnion<N>
 {
 	/**
@@ -23,57 +27,5 @@ public class ArithmeticGenericIntervalsUnion<N>
 	public List<ArithmeticGenericInterval<N>> getIntervals()
 	{
 		return intervals;
-	}
-	
-	@Override
-	public int hashCode()
-	{
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((intervals == null) ? 0 : intervals.hashCode());
-		return result;
-	}
-	
-	@Override
-	public boolean equals(Object obj)
-	{
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		ArithmeticGenericIntervalsUnion other = (ArithmeticGenericIntervalsUnion) obj;
-		if (intervals == null)
-		{
-			if (other.intervals != null)
-				return false;
-		}
-		else if (!intervals.equals(other.intervals))
-			return false;
-		return true;
-	}
-	
-	
-	@Override
-	public String toString()
-	{
-		StringBuilder b = new StringBuilder();
-		boolean first = true;
-		
-		for (ArithmeticGenericInterval<N> i : intervals)
-		{
-			if (first)
-				first = false;
-			else
-				b.append(" ∪ ");
-			
-			b.append(i);
-		}
-		
-		if (first)
-			return "∅";
-			
-		return b.toString();
 	}
 }
